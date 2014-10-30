6972c5c0f034f5c15754310cf1be36ac81687d8d
Mitermayer Reis <mitermayer.reis@gmail.com>
2014-10-30 17:34:57 +1100

    shortcuts creation

--- src/js/shortcut-untangler.js
+++ src/js/shortcut-untangler.js
@@ -1,11 +1,27 @@
-(function (root, factory) {
+(function(root, factory) {
     if (typeof define === 'function' && define.amd) {
         define('shortcutUntangler', [], factory);
     } else {
-        root.shortcutUntangler = factory();
+        root.ShortcutUntangler = factory();
     }
-}(this, function () {
+}(this, function() {
 
+    //var extend = typeof jQuery !== 'undefined' ? jQuery.extend : function(out) {
+    //    out = out || {};
+    //    for (var i = 1; i < arguments.length; i++) {
+    //        if (!arguments[i])
+    //            continue;
+
+    //        for (var key in arguments[i]) {
+    //            if (arguments[i].hasOwnProperty(key))
+    //                out[key] = arguments[i][key];
+    //        }
+    //    }
+
+    //    return out;
+    //};
+
+    // primayKey: key
     var Shortcut = function(options) {
         this.key = options.key;
         this.description = options.description;
@@ -13,27 +29,93 @@
         this.callback = options.callback;
     };
 
+    // primayKey: name
     var Context = function(options) {
         this.description = options.description;
-        this.element = options.element;
         this.name = options.name;
         this.shortcuts = [];
         this.weight = options.weight;
     };
 
+    // primayKey: name
     var Environment = function(options) {
         this.active = false;
         this.context = [];
+        this.description = options.description;
+        this.name = options.name;
     };
 
-    var ShortcutUntanglerFactory = {
-        enableDebug: function(){},
-        changeEnvironment: function(){},
-        createContext: function(){},
-        createEnvironment: function(){},
-        createShortcut: function(){},
-        toJSON: function(){}
+    var _environments = {
+        'main': {
+            'active': true,
+            'description': 'Default shortcuts environment',
+            'name': 'main',
+            'context': [{
+                'description': 'Default shortcuts context',
+                'name': 'main',
+                'weight': 0,
+                'shortcut': {}
+            }]
+        }
     };
 
-    return ShortcutUntanglerFactory;
+    var hasRequiredArguments = function(required, option) {
+        return required.every(function(key) {
+            return typeof option[key] !== 'undefined';
+        });
+    };
+
+    var ShortcutUntanglerFactory = function() {
+        var default_active_envirionment = "main";
+
+        document.getElementsByTagName("body")[0].addEventListener("keypress", function(e) {
+            var key = e.keyCode ? e.keyCode : e.which;
+            var shortcut = String.fromCharCode(key);
+
+            var activeEnvironment = _environments[default_active_envirionment];
+
+            // loop in all context backwards searching for the key
+            for (var i = activeEnvironment.context.length; i > 0; --i) {
+                var _shortcut = activeEnvironment.context[i -1].shortcut[shortcut];
+                if(_shortcut)  {
+                    _shortcut.callback(arguments);
+                    break;
+                }
+            }
+}, false);
+
+        return {
+            changeEnvironment: function() {},
+            enableDebug: function() {},
+            getActiveEnvironment: function() {},
+            createContext: function(option) {
+                var required = ['name', 'description'];
+
+                if (!option || !hasRequiredArguments(required, option)) {
+                    throw new Error("InvalidArguments");
+                }
+            },
+            createEnvironment: function(option) {
+                var required = ['name', 'description'];
+
+                if (!option || !hasRequiredArguments(required, option)) {
+                    throw new Error("InvalidArguments");
+                }
+            },
+            createShortcut: function(option) {
+                var required = ['key', 'callback'];
+
+                if (!option || !hasRequiredArguments(required, option)) {
+                    throw new Error("InvalidArguments");
+                }
+
+                _environments.main.context[0].shortcut[option.key] = option;
+            },
+            toJSON: function() {}
+        };
+    };
+
+    // todo ShortcutUntanglerFactory should be initialized
+
+    return new ShortcutUntanglerFactory();
 }));
--- src/tests/shortcut-untagler.test.js
+++ src/tests/shortcut-untagler.test.js
@@ -1,6 +1,6 @@
-describe('Shortcut untagler API.', function() {
-    var module = window.shortcutUntangler;
+var module = window.ShortcutUntangler;
 
+describe('Interface API', function() {
     it('should provide a method for enable debug mode', function() {
         expect(typeof module.enableDebug).toBe('function');
     });
@@ -20,4 +20,137 @@ describe('Shortcut untagler API.', function() {
     it('should provide a method for generating json', function() {
         expect(typeof module.toJSON).toBe('function');
     });
+
+    it('should provide a method for retriving the active environment', function() {
+        expect(typeof module.toJSON).toBe('function');
+    });
+
+    it('should provide a method for adding context to an existing environment', function() {
+        expect(typeof module.toJSON).toBe('function');
+    });
 });
+
+describe('Defaults', function() {
+    it('should be able to create a shortcut without specifying a context since it should be added to the main context and main environment', function() {
+        var shortcut = jasmine.createSpy('whatAmI');
+
+        module.createShortcut({
+            key: 'e',
+            callback: shortcut
+        });
+
+        triggerNativeKeyHotkey('e');
+
+        expect(shortcut).toHaveBeenCalled();
+    });
+
+
+    // by default we or environment, that should always default to main context / main environment
+    // by default it should already provide a default context and environment
+    // by default we should provide a context called main
+    // by default we should provide and environment called main
+});
+
+describe('Create Context', function() {
+    it('should throw an exception when creating context without arguments', function() {
+        expect(module.createContext).toThrow('InvalidArguments');
+    });
+
+    it('should throw an exception when creating context without a name', function() {
+        expect(function() {
+            module.createContext({
+                description: 'lorem ipsum'
+            });
+        }).toThrow('InvalidArguments');
+    });
+
+    it('should throw an exception when creating context without a description', function() {
+        expect(function() {
+            module.createContext({
+                name: 'foo'
+            });
+        }).toThrow('InvalidArguments');
+    });
+
+    it('should not throw an exception when creating context with name and description', function() {
+        expect(function() {
+            module.createContext({
+                name: 'lorem',
+                description: 'lorem ipsum'
+            });
+        }).not.toThrow('InvalidArguments');
+    });
+});
+
+describe('Create Environment', function() {
+    it('should throw an exception when creating environment without arguments', function() {
+        expect(module.createEnvironment).toThrow('InvalidArguments');
+    });
+
+    it('should throw an exception when creating environment without a name', function() {
+        expect(function() {
+            module.createEnvironment({
+                description: 'lorem ipsum',
+                context: []
+            });
+        }).toThrow('InvalidArguments');
+    });
+
+    it('should throw an exception when creating environment without a description', function() {
+        expect(function() {
+            module.createEnvironment({
+                name: 'foo',
+                context: []
+            });
+        }).toThrow('InvalidArguments');
+    });
+
+});
+
+describe('Create Shortcut', function() {
+    it('should throw an exception when creating shortcut without arguments', function() {
+        expect(module.createShortcut).toThrow('InvalidArguments');
+    });
+
+    it('should throw an exception when creating shortcut without a callback', function() {
+        expect(function() {
+            module.createShortcut({
+                key: 'e'
+            });
+        }).toThrow('InvalidArguments');
+    });
+
+    it('should throw an exception when creating shortcut without a key', function() {
+        expect(function() {
+            module.createShortcut({
+                callback: function(){}
+            });
+        }).toThrow('InvalidArguments');
+    });
+
+    it('should not throw an exception when creating shortcut with key and callback', function() {
+        expect(function() {
+            module.createShortcut({
+                key: 'e',
+                callback: function(){}
+            });
+        }).not.toThrow('InvalidArguments');
+    });
+});
+
+function triggerNativeKeyHotkey(keyCode, el) {
+    var eventObj = document.createEventObject ?  document.createEventObject() : document.createEvent("Events");
+
+    el = el || document.getElementsByTagName('body')[0];
+
+    if(eventObj.initEvent){
+      eventObj.initEvent("keypress", true, true);
+    }
+
+    keyCode = keyCode.charCodeAt(0);
+
+    eventObj.keyCode = keyCode;
+    eventObj.which = keyCode;
+
+    el.dispatchEvent ? el.dispatchEvent(eventObj) : el.fireEvent("onkeydown", eventObj);
+}
