b1a25b689e885962a4305cddd59744c1dd244c1e
Mitermayer Reis <mitermayer.reis@gmail.com>
2014-12-16 22:57:30 +1100

    renaming files in preparation to integrating amd

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,38 @@
 var gulp = require('gulp');
+var del = require('del');
 
 var concat = require('gulp-concat');
 var jshint = require('gulp-jshint');
 var karma = require('gulp-karma');
 var stripDebug = require('gulp-strip-debug');
 var stylish = require('jshint-stylish');
+var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 
 var srcFiles = ['src/js/**/*.js'];
 var testFiles = ['src/tests/**/*.js'];
 var allFiles = srcFiles.concat(testFiles);
 
+
+var DIST = 'dist/';
+var MIN_FILE = 'shortcutUntangler.min.js';
+var SCRIPT_FILE= 'shortcutUntangler.js';
+
+gulp.task('clean', function(cb) {
+  // You can use multiple globbing patterns as you would with `gulp.src`
+  del([DIST], cb);
+});
+
 // JS concat, strip debugging and minify
 gulp.task('scripts', function() {
-    gulp.src(srcFiles)
-        .pipe(concat('shortcut-untangler.min.js'))
-        .pipe(stripDebug())
-        .pipe(uglify())
-        .pipe(gulp.dest('./'));
+    del([DIST + MIN_FILE], function() {
+        gulp.src(srcFiles)
+            .pipe(stripDebug())
+            .pipe(uglify())
+            .pipe(concat(MIN_FILE))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest('dist'));
+    });
 });
 
 gulp.task('lint', function() {
